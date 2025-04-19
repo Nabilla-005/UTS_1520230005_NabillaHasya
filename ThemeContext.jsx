11fe444992d6fc0fd5650fa5ca6550f4c7cd646b
Nabilla Hasya <nabilla.hasya@mhs.itenas.ac.id>
2025-04-19 21:59:32 +0700

    Add files via upload

---
+++ ThemeContext.jsx
@@ -0,0 +1,34 @@
+// src/Components/ThemeContext.jsx
+import React, { createContext, useState, useEffect } from "react";
+
+export const ThemeContext = createContext();
+
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(() => {
+    if (localStorage.getItem("theme")) {
+      return localStorage.getItem("theme");
+    }
+    return "light"; // default
+  });
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    if (theme === "dark") {
+      root.classList.add("dark");
+    } else {
+      root.classList.remove("dark");
+    }
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  const toggleTheme = () =>
+    setTheme((prev) => (prev === "dark" ? "light" : "dark"));
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
